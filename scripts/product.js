4113462e07839c604af72e7148b8df8ad0eb9c50
junmarktimon <72652106+junmarktimon@users.noreply.github.com>
2020-10-25 00:27:33 +0800

    Delete product.js

--- scripts/product.js
+++
@@ -1,9 +0,0 @@
-angular.module('shopApp', [])
- .controller('productController', function ($scope) {
- var id = +utils.getQueryParams()['id'],
- p = repository.getProduct(id);
- $scope.name = p.name;
- $scope.price = p.price;
- $scope.description = p.description;
- $scope.category = p.category;
- });
